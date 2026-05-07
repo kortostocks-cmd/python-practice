43f3e8a0695b01102635962edd88a072252ad685
kortostocks-cmd <kortostocks@gmail.com>
2026-05-07 10:32:39 -0500

    miercoles 6 de mayo

--- Aprendizaje/python_base/5Python_function.py
+++ Aprendizaje/python_base/5Python_function.py
@@ -44,5 +44,30 @@ productos.sort(key=lambda x: x["precio"], reverse=True)
 
 print(productos)
 
+#todo junto
+precios_con_itbms = [100, 107, 50, 53.5, 20, 21.4]
 
+def obtener_solo_impuestos(lista):
+    # Asumiendo que el precio base es múltiplo de 100 y el impuesto es 7%
+    # Filtramos los que NO son enteros redondos o tienen el decimal del 7%
+    return list(filter(lambda p: p % 1 != 0, lista))
 
+# Si lo que quieres es filtrar una lista de diccionarios por una categoría:
+productos = [
+    {"nombre": "A", "precio": 50, "tiene_impuesto": False},
+    {"nombre": "B", "precio": 20, "tiene_impuesto": True},
+    {"nombre": "C", "precio": 100, "tiene_impuesto": True}
+]
+
+def filtrar_con_impuesto(lista):
+    return list(filter(lambda x: x["tiene_impuesto"] == True, lista))
+
+print(filtrar_con_impuesto(productos))
+
+ #TODO JUNTO
+precios_con_itbms = [100, 107, 50, 53.5, 20, 21.4]
+
+def obtener_solo_impuestos(lista):
+    return list(filter(lambda p: p % 1 != 0, lista))
+
+print(obtener_solo_impuestos(precios_con_itbms))
